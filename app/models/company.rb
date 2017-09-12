0d7c53943281820d28db685b73c10cb3a472f019
Casey Ann Cumbow <cumbow8@gmail.com>
2017-09-12 10:52:21 -0400

    Remove validation on a Company's name as it's not in the requirements

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,6 @@
 class Company < ApplicationRecord
   PLAN_LEVELS = %w(legacy custom basic plus growth enterprise)
 
-  validates :name, presence: true
   validates :plan_level, inclusion: { in: PLAN_LEVELS}
 
   enum plan_level: PLAN_LEVELS
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 describe Company do
   context "validations" do
-    it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to define_enum_for(:plan_level).with([:legacy, :custom, :basic, :plus, :growth, :enterprise]) }
   end
 end

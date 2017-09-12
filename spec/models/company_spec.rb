8c3bf3665fe4bbfdef6a2e136a3a418f26d12f4e
Casey Ann Cumbow <cumbow8@gmail.com>
2017-09-12 13:17:55 -0400

    Add with_modern_plan query method in Company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -14,4 +14,14 @@ describe Company do
       expect(Company.sort_alphabetically).to eq([abc, penelope, zebra])
     end
   end
+
+  describe ".with_modern_plan" do
+    it "returns a list of companies with a modern plan" do
+      penelope = Company.create!(name: "Penelope's", plan_level: "enterprise")
+      abc = Company.create!(name: "ABC's", plan_level: "basic")
+      zebra = Company.create!(name: "Zebra Company", plan_level: "legacy")
+
+      expect(Company.with_modern_plan).to include(abc, penelope)
+    end
+  end
 end

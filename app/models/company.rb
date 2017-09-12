8b4d69da6c074ec4d3bba708860df1cafb4fcd7f
Casey Ann Cumbow <cumbow8@gmail.com>
2017-09-12 13:05:54 -0400

    Add /companies/alphabetically endpoint, Company class method, specs

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,8 @@ class Company < ApplicationRecord
   validates :plan_level, inclusion: { in: PLAN_LEVELS}
 
   enum plan_level: PLAN_LEVELS
+
+  def self.sort_alphabetically
+    order(:name)
+  end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,4 +4,14 @@ describe Company do
   context "validations" do
     it { is_expected.to define_enum_for(:plan_level).with([:legacy, :custom, :basic, :plus, :growth, :enterprise]) }
   end
+
+  describe ".sort_alphabetically" do
+    it "returns a list of companies sorted alphabetically by name" do
+      penelope = Company.create!(name: "Penelope's", plan_level: "enterprise")
+      abc = Company.create!(name: "ABC's", plan_level: "enterprise")
+      zebra = Company.create!(name: "Zebra Company", plan_level: "enterprise")
+
+      expect(Company.sort_alphabetically).to eq([abc, penelope, zebra])
+    end
+  end
 end

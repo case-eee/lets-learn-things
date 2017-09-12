297e0bcd2e518b724322f6bf1220722f71b839e6
Casey Ann Cumbow <cumbow8@gmail.com>
2017-09-12 13:18:08 -0400

    Add endpoint and spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :companies, only: [:index]
       get "/companies/alphabetically" => "companies#alphabetically"
+      get "/companies/with_modern_plan" => "companies#with_modern_plan"
     end
   end
 end

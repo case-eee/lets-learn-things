0022f5f440a29efef532feefc831820132ccf2a7
Casey Ann Cumbow <cumbow8@gmail.com>
2017-09-12 12:14:47 -0400

    Fix Lesson model spacing

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,11 +1,11 @@
 class Lesson < ApplicationRecord
-	validates :name, presence: true
-	validate :name_contains_only_letters_and_numbers
-	belongs_to :company
+  validates :name, presence: true
+  validate :name_contains_only_letters_and_numbers
+  belongs_to :company
 
-	def name_contains_only_letters_and_numbers
-		if !(self.name =~ /^[a-zA-Z0-9]+$/)
-			errors.add(:name_content, "must only include letters and numbers")
-		end
-	end
+  def name_contains_only_letters_and_numbers
+    if !(self.name =~ /^[a-zA-Z0-9]+$/)
+      errors.add(:name_content, "must only include letters and numbers")
+    end
+  end
 end
